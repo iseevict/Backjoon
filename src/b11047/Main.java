ae5edf56ac5dd98004c19b5c2cc36bbaff91f0ac
stpark0059 <stpark0509@gmail.com>
2024-05-12 22:24:22 +0900

    백준(솔브닥 class3)

---
+++ src/b11047/Main.java
@@ -0,0 +1,18 @@
+package b11047;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, K;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(buf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+    }
+}
+
