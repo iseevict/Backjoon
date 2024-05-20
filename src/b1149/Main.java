b9dffa735734ca220379fe01829b8e1fbad67dcf
stpark0059 <stpark0509@gmail.com>
2024-05-20 19:59:57 +0900

    백준(솔브닥 class4) 1149 SOL

--- src/b1149/Main.java
+++ src/b1149/Main.java
@@ -9,7 +9,18 @@ public class Main {
         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(buf.readLine());
+        int[][] rgb = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(buf.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
+            rgb[i][0] = Math.min(rgb[i - 1][1], rgb[i - 1][2]) + r;
+            rgb[i][1] = Math.min(rgb[i - 1][0], rgb[i - 1][2]) + g;
+            rgb[i][2] = Math.min(rgb[i - 1][0], rgb[i - 1][1]) + b;
+        }
+        System.out.println(Math.min(rgb[N][0], Math.min(rgb[N][1], rgb[N][2])));
     }
 }
 
